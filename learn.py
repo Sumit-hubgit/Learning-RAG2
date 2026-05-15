9ab972aea9f7eb5bdd19d18fdc021c6828d714ae
Sumit-hubgit <sumitsahu7017@gmail.com>
2026-05-16 00:08:53 +0530

    Complete pipeline. added th model

--- learn.py
+++ learn.py
@@ -10,7 +10,7 @@ directory_load  = DirectoryLoader(
     loader_cls=PyMuPDFLoader,
     show_progress=True
 )
-doc = directory_load.load()  #  it returns list of documents where eah document contains the metadata and the page_content
+doc = directory_load.load()  #  it returns list of documents where each document contains the metadata and the page_content
 # print(type(doc))
 # for content in doc:
 #     print(content.metadata)
@@ -27,3 +27,7 @@ print(f"Chunk metadata: {chunks[0].metadata} \n Chunks content {chunks[0].page_c
 
 model  = SentenceTransformer("all-MiniLM-L6-v2")
 print(model.get_embedding_dimension())
+
+texts = [ doc.page_content for doc in chunks]
+encodings = model.encode(texts)
+print(encodings)
